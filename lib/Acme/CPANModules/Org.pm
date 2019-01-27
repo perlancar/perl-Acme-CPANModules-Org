ade42461cf8105dc5951f81c3dafa3e3f09a2d98
perlancar (on pc-home) <perlancar@gmail.com>
2019-01-27 07:45:59 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/Org.pm
@@ -0,0 +1,35 @@
+package Acme::CPANModules::Org;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules related to Org format",
+    description => <<'_',
+
+
+_
+    entries => [
+        {module=>'Org::Parser'},
+        {module=>'Org::Dump'},
+        {module=>'App::OrgUtils'},
+        {module=>'Org::To::HTML'},
+        {module=>'Org::To::Pod'},
+        {module=>'Org::To::VCF'},
+        {module=>'Org::To::HTML::WordPress'},
+        {module=>'Org::To::Text'},
+        {module=>'App::orgsel'},
+        {module=>'Text::Table::Org'},
+        {module=>'Data::CSel'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<https://orgmode.org>
